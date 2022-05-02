9dd186a1114f5d66f6867ee21b384b538fb69416
IT Admin <91905626+IT-Administrators@users.noreply.github.com>
2022-05-02 18:28:08 +0200

    Update Function_PingTimeControlled.ps1

--- Function_PingTimeControlled.ps1
+++ Function_PingTimeControlled.ps1
@@ -3,15 +3,15 @@ Author: IT-Administrators
 Powershell Version: 5.1.19041.1237
 #>
 <#Function for checking connection to server. The ping command is used timebased. 
-I use this function to monitor the isp connection. With the out-file cmdlet you can export the results to a file.
+I use this function to monitor the isp connection. With the <Out-File> cmdlet you can export the results to a file.
 If the connection is lost the function stops so there's no more input to the file.#>
 function CheckPingToISP {
     param (
         [string]$ServerName = "www.heise.de"
     )
     #$Result = "Connection to ISP failed"
-    <#Using the .net class to send the ping is much more faster than using ping or Test-Connection.
-    You can check this with Measure-Command.#>
+    <#Using the .Net class to send the ping is much more faster than using ping or <Test-Connection>.
+    You can check this with <Measure-Command>.#>
     $Ping = [System.Net.NetworkInformation.Ping]::new()
     $Reply = $Ping.Send($ServerName)
     Remove-Item C:\Temp\ISPConnectionMonitor.txt
@@ -22,7 +22,7 @@ function CheckPingToISP {
     else{
         While ($Reply.Status -eq "Success")
         {
-            #The start-sleep cmdlet is used to ping every 2 seconds. We need this to prevent huge traffic from the $Ping.Send() method.
+            #The <Start-Sleep> cmdlet is used to ping every 2 seconds. We need this to prevent huge traffic from the $Ping.Send() method.
             Start-Sleep -Seconds 2
             $TodaysPing = Get-Date
             $Result = Write-Output "ping to $ServerName Passed $TodaysPing" | Out-File C:\Temp\ISPConnectionMonitor.txt -Append
