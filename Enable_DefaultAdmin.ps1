1d212dce96226765538a2f35f20ca60136c7daf5
IT Admin <91905626+IT-Administrators@users.noreply.github.com>
2022-05-02 18:11:25 +0200

    Update Enable_DefaultAdmin.ps1

--- Enable_DefaultAdmin.ps1
+++ Enable_DefaultAdmin.ps1
@@ -2,7 +2,7 @@
 Author: IT-Administrators
 Powershell Version: 5.1.19041.1237
 #>
-<#This script activates the default admin. It uses cmd comamnds to to this because there's no powershell way
+<#This script activates the default admin. It uses cmd commands because there's no powershell way
 to do this. Change the [passwort] with the password you want.#>
 "Activate defaul admininistrator"
 ''
