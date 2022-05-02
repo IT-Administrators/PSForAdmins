5b2ee682759614f66cf8e7594e244618fd76faed
IT Admin <91905626+IT-Administrators@users.noreply.github.com>
2022-05-02 18:29:19 +0200

    Update Function_SendMailMessageGithub.ps1

--- Function_SendMailMessageGithub.ps1
+++ Function_SendMailMessageGithub.ps1
@@ -2,14 +2,14 @@
 Author: IT-Administrators
 Powershell Version: 5.1.19041.1237
 #>
-<#Function for sending Mail. This function is used by calling SendMailMessage.
+<#Function for sending mail. This function is used by calling <SendMailMessage>.
 With the parameter switches you can adjust this function to your needs for example:
-SendMailMessage -SMTPServer ExampleServer#>
+>SendMailMessage -SMTPServer ExampleServer>#>
 function SendMailMessage{
     #Parameters for the function. Change them as you need.
     param(
     <#You have to change to your smtp server. You have two options to do that. 
-    $PSEmailServer = "smtp.exampleserver.com" or you change the parameter. #>
+    <$PSEmailServer = "smtp.exampleserver.com"> or you change the parameter. #>
     $SMTPServer = $PSEmailServer,
     $SendFrom = "Examplemail@PowerShellMailService.com",
     $SendTo = "Examplemail@PowerShellMailService.com",
