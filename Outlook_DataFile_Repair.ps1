db474dfe75a9cfaff6666fa61672b8873573b1a2
IT Admin <91905626+IT-Administrators@users.noreply.github.com>
2022-05-04 16:44:58 +0200

    Update Outlook_DataFile_Repair.ps1

--- Outlook_DataFile_Repair.ps1
+++ Outlook_DataFile_Repair.ps1
@@ -4,8 +4,8 @@ Powershell Version: 5.1.19041.1237
 #>
 "Outlook data file repair"
 ''
-<#Windows has an implemented program to scan and fix datafiles in outlook, the directory of the file is not explicit, it depends on the os you are using, the office you are using etc.
-Scanning the system for the specified string on C:\-, the recurse parameter is necessary to scan every file and folder on C:\ not just the current directory.
+<#Windows has an implemented program to scan and fix datafiles in outlook, the directory of the file is not explicit, it depends on the os you are using, the office version you are using etc.
+Scanning the system for the specified string on C:\, the recurse parameter is necessary to scan every file and folder on C:\ not just the current directory.
 This script needs to be run elevated. #>
 $ScanPst = Get-ChildItem C:\ -Filter "Scanpst.exe" -Recurse |  Select-Object {$_.DirectoryName} -ErrorAction SilentlyContinue
 ''
