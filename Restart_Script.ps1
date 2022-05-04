b7da4fbc1c4207c1e36c301af9640f29b81e1bcd
IT Admin <91905626+IT-Administrators@users.noreply.github.com>
2022-05-04 16:46:43 +0200

    Update Restart_Script.ps1

--- Restart_Script.ps1
+++ Restart_Script.ps1
@@ -3,10 +3,10 @@ Author: IT-Administrators
 Powershell Version: 5.1.19041.1237
 #>
 <#This script restarts another script for the specified time. 
-With the -StartScript parameter you can call this function and specifie your own script. 
-The -ExecutionPolicy parameter sets the execution policy. Default is bypass to run this on every device without chaging 
-execution policy. To change the timespan this script is run you have to change the $Timer variable.
-The Start-Sleep cmdlet sets the refreshing time of the console. By changing this you can refresh more often.#>
+With the <StartScript> parameter you can call this function and specify your own script. 
+The <ExecutionPolicy> parameter sets the execution policy. Default is bypass to run this on every device without changing 
+execution policy. To change the timespan this script is run you have to change the <$Timer> variable.
+The <Start-Sleep> cmdlet sets the refreshing time of the console. By changing this you can refresh more often.#>
 $Timer = New-TimeSpan -Days 365
 $Clock = [diagnostics.stopwatch]::StartNew()
 function RestartScript{
