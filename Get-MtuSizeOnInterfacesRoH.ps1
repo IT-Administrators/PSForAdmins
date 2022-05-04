b41cb62c73607fd679c5813a526c006bf502e3e2
IT Admin <91905626+IT-Administrators@users.noreply.github.com>
2022-05-04 16:05:45 +0200

    Update Get-MtuSizeOnInterfacesRoH.ps1

--- Get-MtuSizeOnInterfacesRoH.ps1
+++ Get-MtuSizeOnInterfacesRoH.ps1
@@ -1,12 +1,12 @@
 ﻿<#
 .Synopsis
-   Get MTU settings
+   Get MTU settings.
 
 .DESCRIPTION
-   Get MTU settings of one or every adapter. 
+   Get MTU settings of one or every net adapter. 
 
 .EXAMPLE
-   .\Get-MtuSizeOfInterfaces.ps1 -GetAllInterfaceMTU
+   .\Get-MtuSizeOnInterfacesRoH.ps1 -GetAllInterfaceMTU
 
     ifIndex InterfaceAlias                  AddressFamily NlMtu(Bytes) InterfaceMetric Dhcp     ConnectionState PolicyStore
     ------- --------------                  ------------- ------------ --------------- ----     --------------- -----------
@@ -16,26 +16,22 @@
     35      Ethernet 2                      IPv6                  1500              25 Enabled  Connected       ActiveStore
     15      Ethernet 3                      IPv4                  1400               2 Enabled  Disconnected    ActiveStore
     15      Ethernet 3                      IPv6                  1400               2 Enabled  Disconnected    ActiveStore
-    6       LAN-Verbindung* 1               IPv4                  1500              25 Enabled  Disconnected    ActiveStore
-    6       LAN-Verbindung* 1               IPv6                  1500              25 Disabled Disconnected    ActiveStore
-    11      LAN-Verbindung* 2               IPv6                  1500              25 Disabled Disconnected    ActiveStore
-    11      LAN-Verbindung* 2               IPv4                  1500              25 Enabled  Disconnected    ActiveStore
-    1       Loopback Pseudo-Interface 1     IPv4            4294967295              75 Disabled Connected       ActiveStore
-    1       Loopback Pseudo-Interface 1     IPv6            4294967295              75 Disabled Connected       ActiveStore
-    20      Mobilfunk                       IPv6                  1500              25 Disabled Disconnected    ActiveStore
-    20      Mobilfunk                       IPv4                  1500              25 Disabled Disconnected    ActiveStore
+    6       LAN                             IPv4                  1500              25 Enabled  Disconnected    ActiveStore
+    6       LAN                             IPv6                  1500              25 Disabled Disconnected    ActiveStore
+    1       Loopback                        IPv4            4294967295              75 Disabled Connected       ActiveStore
+    1       Loopback                        IPv6            4294967295              75 Disabled Connected       ActiveStore
     32      WLAN                            IPv6                  1500              25 Enabled  Disconnected    ActiveStore
     32      WLAN                            IPv4                  1400              25 Enabled  Disconnected    ActiveStore
 
 .EXAMPLE
-   .\Get-MtuSizeOfInterfaces.ps1 -GetConnectedInterfaceMTU
+   .\Get-MtuSizeOnInterfacesRoH.ps1 -GetConnectedInterfaceMTU
 
     ifIndex InterfaceAlias                  AddressFamily NlMtu(Bytes) InterfaceMetric Dhcp     ConnectionState PolicyStore
     ------- --------------                  ------------- ------------ --------------- ----     --------------- -----------
     35      Ethernet 2                      IPv4                  1500              25 Enabled  Connected       ActiveStore
     35      Ethernet 2                      IPv6                  1500              25 Enabled  Connected       ActiveStore
-    1       Loopback Pseudo-Interface 1     IPv4            4294967295              75 Disabled Connected       ActiveStore
-    1       Loopback Pseudo-Interface 1     IPv6            4294967295              75 Disabled Connected       ActiveStore
+    1       Loopback                        IPv4            4294967295              75 Disabled Connected       ActiveStore
+    1       Loopback                        IPv6            4294967295              75 Disabled Connected       ActiveStore
 
 .NOTES
    Written and testet in PowerShell 5.1.
