795637c9892d072c9a43b15b80a53d0d2f4603f9
IT Admin <91905626+IT-Administrators@users.noreply.github.com>
2022-05-04 16:10:14 +0200

    Update Get_ADComputerIPs.ps1

--- Get_ADComputerIPs.ps1
+++ Get_ADComputerIPs.ps1
@@ -4,8 +4,8 @@ Powershell Version: 5.1.19041.1237
 #>
 <#Retrieving dns host name of every enabled ad computer than resolving the dns name to get the client ip address. 
 The outcome is sort by dns host name. By adding a sort object to the foreach you can sort outcome by ip address.
-Erroraction variable is used to prevent errormessages if the client domain name is not present.
-By uncommenting the select object from the resolve cmdlet you can export the results to a csv file.#>
+Erroraction variable is used to prevent errormessages if the client domain is not present.
+By uncommenting the select object behind the resolve cmdlet you can export the results to a csv file.#>
 $ADComputername = Get-ADComputer -Filter 'Enabled -eq $true' -Properties DnsHostName | Select-Object DnsHostName | Sort-Object DnsHostName
 #Remove-Item C:\Temp\ADComputerIPs.csv
 foreach ($_ in $ADComputername.DnsHostName){
