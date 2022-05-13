5e97d3694a136c11a47b17c76bc9465a036e178c
IT Admin <91905626+IT-Administrators@users.noreply.github.com>
2022-05-13 15:07:12 +0200

    Add files via upload
    This script gets active directory group member. For further information use Get-Help Get-ADGroupMemberRoH.ps1 -Full.

---
+++ Get-ADGroupMemberRoH.ps1
@@ -0,0 +1,66 @@
+﻿<#
+.Synopsis
+    Get active directory group member.
+
+.DESCRIPTION
+    This script gets all member of all active directory groups related to a keyword or member of a specified 
+    active directory group.
+
+.EXAMPLE
+    .\Get-ADGroupMemberRoH.ps1 -ADGroupsByKeyword "Finance"
+
+    Member of Group: Finance_R
+    ----------------
+    GS-Example
+    GS-Example2
+    GS-Example3
+
+    Member of Group: Finance_RW
+    ----------------
+    GS-Example
+    GS-Example2
+
+    ...
+    
+.EXAMPLE
+    .\Get-ADGroupMemberRoH.ps1 -SpecificADGroup "Finance_RW"
+    
+    Member of Group: Finance_RW
+    ----------------
+    GS-Example
+    GS-Example2
+
+.NOTES
+    Written and testet in PowerShell 5.1.
+
+.LINK
+    https://github.com/IT-Administrators/PSForAdmins/tree/PowerShell-5.1
+#>
+
+[CmdletBinding(DefaultParameterSetName='ADGroupMemberByGroupKeyword', 
+               SupportsShouldProcess=$true)]
+param(
+    [Parameter(
+    ParameterSetName='ADGroupMemberByGroupKeyword',
+    Position=0,
+    HelpMessage='Every member of every group related to your keyword.')]
+    [String]$ADGroupsByKeyword,
+
+    [Parameter(
+    ParameterSetName='ADGroupMember',
+    Position=0,
+    HelpMessage='AD group name.')]
+    [String]$SpecificADGroup
+)
+
+if($ADGroupsByKeyword){
+    $ADGroupMember = Get-ADGroup -Filter * -Properties * | Where-Object Name -Like "*$ADGroupsByKeyword*"
+    $ADGroupMember.SamAccountName |  ForEach-Object{
+        Write-Output "" "Member of Group: $_" "----------------"
+        Get-ADGroupMember -Identity $_ | Select-Object SamAccountName -ExpandProperty SamAccountName
+    }
+}
+if($SpecificADGroup){
+    Write-Output "" "Member of Group: $SpecificADGroup" ""
+    Get-ADGroupMember -Identity "$SpecificADGroup" | Select-Object SamAccountName -ExpandProperty SamAccountName
+}
