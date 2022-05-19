98dc7c19895559fdcb7e7e1a79c1743624e80539
IT Admin <91905626+IT-Administrators@users.noreply.github.com>
2022-05-19 08:31:35 +0200

    Add files via upload
    This Powershell snippet creates a random password with the length of 8.

---
+++ Create_RandomPassword.ps1
@@ -0,0 +1,8 @@
+﻿<#
+Author: IT-Administrators
+Powershell Version: 5.1.19041.1237
+#>
+#Generate random pw with specified length
+[System.Reflection.Assembly]::LoadWithPartialName("System.Web")
+#PW with 8 characters and 2 non letters or numbers
+[System.Web.Security.Membership]::GeneratePassword(8,2)
