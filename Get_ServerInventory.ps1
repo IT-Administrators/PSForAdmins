224d8f7e9e1a8b308969546a3e9c0c1f01e8a5ba
IT Admin <91905626+IT-Administrators@users.noreply.github.com>
2022-07-20 16:45:20 +0200

    Add files via upload
    This script gets all windows server in you inventory and exports them to a csv file.

---
+++ Get_ServerInventory.ps1
@@ -0,0 +1,25 @@
+﻿<#
+Author: IT-Administrators
+Powershell Version: 5.1.19041.1237
+#>
+"Get server inventory"
+$ADComputer = Get-ADComputer -Filter{OperatingSystem -like "*server*"} -Properties * | Select-Object Name,IPv4Address | Sort-Object Name
+
+Invoke-Command -ComputerName ($ADComputer.Name) -ScriptBlock {
+
+    $Servername = $env:COMPUTERNAME
+    $IPAddress = (Get-NetIPConfiguration).IPv4Address.IPAddress
+    $MacAddress = (Get-NetAdapter).MacAddress
+    $DefaultIPv4GWNextHop = (Get-NetIPConfiguration).IPv4Defaultgateway.NextHop
+    $DefaultIPv6GWNextHop = (Get-NetIPConfiguration).IPv6Defaultgateway.NextHop
+
+    $InfoObject = New-Object PSObject
+
+    Add-Member -InputObject $InfoObject -MemberType NoteProperty -Name "ServerName" -Value $Servername
+    Add-Member -InputObject $InfoObject -MemberType NoteProperty -Name "IP Address" -Value $IPAddress
+    Add-Member -InputObject $InfoObject -MemberType NoteProperty -Name "MAC Address" -Value $MacAddress
+    Add-Member -InputObject $InfoObject -MemberType NoteProperty -Name "IPv4 Gateway" -Value $DefaultIPv4GWNextHop
+    Add-Member -InputObject $InfoObject -MemberType NoteProperty -Name "IPv6 Gateway" -Value $DefaultIPv6GWNextHop
+
+    $InfoObject
+} | Select-Object * -ExcludeProperty PSComputerName, RunspaceId, PSShowComputerName | Export-Csv -Path C:\Temp\Server_Inventory_$((Get-Date).ToString('MM-dd-yyyy')).csv -NoTypeInformation -Delimiter ";" -Force
