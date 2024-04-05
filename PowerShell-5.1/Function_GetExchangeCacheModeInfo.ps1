9cc81254f8eb27566b2351e4de65d070f396edd6
IT Admin <91905626+IT-Administrators@users.noreply.github.com>
2024-04-05 15:27:40 +0200

    Add files via upload

---
+++ PowerShell-5.1/Function_GetExchangeCacheModeInfo.ps1
@@ -0,0 +1,37 @@
+﻿<#
+.Synopsis
+    Get information about exchange cache mode.
+
+.DESCRIPTION
+    Get information about exchange cade mode.
+
+    Because the cachemode is a client configuration, this script must be run on the client
+    and requires the outlook app installed.
+
+.EXAMPLE
+    Check if exchange cache mode is enabled. 
+
+    Get-ExchangeCacheModeInfoRoH
+
+.NOTES
+    Written and testet in PowerShell 5.1.
+
+.LINK
+    https://github.com/IT-Administrators/PSForAdmins/tree/main/PowerShell-5.1
+#>
+
+function Get-ExchangeCacheModeInfoRoH {
+    
+    # Create outlook com object to access outlook settings.
+    $OutlookObj = New-Object -ComObject Outlook.Application
+    $Namespace = $OutlookObj.GetNamespace("MAPI")
+    # Get cache mode infos and store in pscustomobject.
+    $Namespace.Stores | ForEach-Object {
+        $CacheModeObj = New-Object PSCustomObject
+        Add-Member -InputObject $CacheModeObj -MemberType NoteProperty -Name MailboxName -Value $_.DisplayName
+        Add-Member -InputObject $CacheModeObj -MemberType NoteProperty -Name CacheModeEnabled -Value $_.IsCachedExchange
+        Add-Member -InputObject $CacheModeObj -MemberType NoteProperty -Name DataFileStoreEnabled -Value $_.IsDataFileStore
+        Add-Member -InputObject $CacheModeObj -MemberType NoteProperty -Name FilePath -Value $_.FilePath
+        $CacheModeObj
+    }
+}
