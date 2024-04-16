bf8f5d2a52ca1e63d1b003690f43b08186720a1c
IT Admin <91905626+IT-Administrators@users.noreply.github.com>
2024-04-16 14:12:52 +0200

    Add files via upload
    New function to connect to bastion host via RDP using powershell.

---
+++ PowerShell-5.1/Function_ConnectBastionHostRDP.ps1
@@ -0,0 +1,182 @@
+﻿<#
+.Synopsis
+    Connects via RDP to a bastion host
+
+.DESCRIPTION
+    Connect via RDP to a bastion host located inside your azure.
+
+    Use the <CheckPrerequisites> parameter to install or update the required modules.
+.EXAMPLE
+    Connect to bastion host via RDP.
+
+    Connect-AzureBastionHostRDP -AzureVmName "ExampleVM" -BastionHostName "Example-Bastion" -TenantId "xxxxxxxx" -BastionSubscriptionId "xxxxxxxx" -BastionResoureGroup "ExampleDomain-RSG-Bastion" -Verbose
+    
+    Output:
+
+    VERBOSE: Sent top=100 skip=0 skipToken=
+    VERBOSE: Received results: 1
+    VERBOSE: Found the VM.
+
+    VERBOSE: Connected to Bastion Example-Bastion
+    VERBOSE: Creating RDD profile.
+    VERBOSE: GET with 0-byte payload
+    VERBOSE: received -1-byte response of content type text/plain
+    VERBOSE: Deleting the RDP file after use.
+    VERBOSE: Performing the operation "Remove File" on target "%userprofile%\Desktop\ExampleVM-2024-04-16@140441.rdp".
+    VERBOSE: Deleted %userprofile%\Desktop\ExampleVM-2024-04-16@140441.rdp.
+
+.NOTES
+    Written and testet in PowerShell 5.1.
+
+.LINK
+    https://github.com/IT-Administrators/PSForAdmins/tree/main/PowerShell-5.1
+#>
+
+function Connect-AzureBastionHostRDP {
+
+    [CmdletBinding(DefaultParameterSetName='ConnectAzureBastionHost', 
+                   SupportsShouldProcess=$true)]
+
+    param(
+        [Parameter(
+        ParameterSetName='ConnectAzureBastionHost',
+        Position=0,
+        HelpMessage='Azure VM name.')]
+        [ValidateNotNullOrEmpty()]
+        [string]$AzureVmName,
+
+        [Parameter(
+        ParameterSetName='ConnectAzureBastionHost',
+        Position=0,
+        HelpMessage='Bastion hostname.')]
+        [string]$BastionHostName,
+
+        [Parameter(
+        ParameterSetName='ConnectAzureBastionHost',
+        Position=0,
+        HelpMessage='TenantId.')]
+        [string]$TenantId,
+
+        [Parameter(
+        ParameterSetName='ConnectAzureBastionHost',
+        Position=0,
+        HelpMessage='Bastion subscription id.')]
+        [string]$BastionSubscriptionId,
+
+        [Parameter(
+        ParameterSetName='ConnectAzureBastionHost',
+        Position=0,
+        HelpMessage='Bastion resource group.')]
+        [string]$BastionResoureGroup,
+
+        [Parameter(
+        ParameterSetName='ConnectAzureBastionHostModuleCheck',
+        Position=0,
+        HelpMessage='Required modules will be installed or updated.')]
+        [switch]$CheckPrerequisites
+    )
+
+    if ($CheckPrerequisites){
+        $Modules = @("az","az.resourcegraph")
+        # Verify that modules are installed.
+        foreach($module in $Modules){
+            $ModuleCheck = Get-Module -Name $module
+            if ($ModuleCheck -ne $true){
+                Find-Module -Name $module -Verbose | Install-Module -Scope CurrentUser -Verbose -Force
+                Import-Module -Name $module -Verbose
+            }
+            else{
+                Update-Module -Name $module -Verbose
+                Import-Module -Name $module -Verbose
+            }
+        }
+    }
+    else{
+        # Import-Module -Name $Modules -Verbose
+        Connect-AzAccount -Tenant $TenantId -Subscription $BastionSubscriptionId | Out-Null
+
+        # Query azure to find vm and get information about it.
+        $VMToConnectTo = Search-AzGraph -Query "resources | extend PowerState = tostring(properties.extended.instanceView.powerState.code) | where type == 'microsoft.compute/virtualmachines' and name == '$AzureVmName'" -UseTenantScope
+        $VmResourceId = $VMToConnectTo.ResourceId
+
+        if (!([string]::IsNullOrEmpty($VmResourceId))) {
+            Write-Verbose -Message "Found the VM." -Verbose
+            # Check if machine is running otherwise exit.
+            if ($VMToConnectTo.PowerState -eq 'PowerState/deallocated') {
+                $User = [Environment]::UserName
+                Write-Verbose -Message "The Vm is not running. Please start the vm." -Verbose
+            }
+            else {
+                # Connect to the bastion sub in the correct tenant.
+                Select-AzSubscription $BastionSubscriptionId -Tenant  $TenantId | Out-Null
+                # Get azure access token.
+                $AccessToken = (Get-AzAccessToken).Token
+                if (!([string]::IsNullOrEmpty($AccessToken))) {
+                    try {
+                        # Get bastion information.
+                        $Bastion = Get-AzBastion -ResourceGroupName $BastionResoureGroup -Name $BastionHostName
+                        if ($null -ne $Bastion) {
+                            Write-Verbose -Message "Connected to Bastion $($Bastion.Name)" -Verbose
+                            Write-Verbose -Message "Creating RDD profile." -Verbose
+                            
+                            # Create endpoint (url) informations. This is later used to create the rdp file.
+                            $TargetResourceId = $VmResourceId
+                            $EnableMFA = "true"
+                            $BastionEndpoint = $Bastion.DnsName
+                            $ResourcePort = "3389"
+
+                            $Url = "https://$($BastionEndpoint)/api/rdpfile?resourceId=$($TargetResourceId)&format=rdp&rdpport=$($ResourcePort)&enablerdsaad=$($EnableMFA)"
+
+                            # Create authentication headers.
+                            $Headers = @{
+                                "Authorization"   = "Bearer $($AccessToken)"
+                                "Accept"          = "*/*"
+                                "Accept-Encoding" = "gzip, deflate, br"
+                                #"Connection" = "keep-alive" #keep-alive and close not supported with PoSh 5.1 
+                                "Content-Type"    = "application/json"
+                            }
+                            # Create RDP file informations.
+                            $DesktopPath = [Environment]::GetFolderPath("Desktop")
+                            $DateStamp = Get-Date -Format yyyy-MM-dd
+                            $TimeStamp = Get-Date -Format HHmmss
+                            $DateAndTimeStamp = $DateStamp + '@' + $TimeStamp 
+                            $RdpPathAndFileName = "$DesktopPath\$AzureVmName-$DateAndTimeStamp.rdp"
+                            $ProgressPreference = 'SilentlyContinue'
+                        }
+                        else {
+                            Write-Verbose -Message "We could not connect to azure bastion host." -Verbose
+                        }
+                    }
+                    catch {
+                        Write-Error -Message "Internal error." -Category ObjectNotFound
+                    }
+                    
+                    try {
+                        $ProgressPreference = 'SilentlyContinue'
+                        # Get RDP configfile informations.
+                        Invoke-WebRequest $Url -Method Get -Headers $Headers -OutFile $RdpPathAndFileName -UseBasicParsing
+                        $ProgressPreference = 'Continue'
+                        
+                        # Check if RDP config file exist and if yes start it.
+                        if (Test-Path $RdpPathAndFileName -PathType leaf) {
+                            Start-Process $RdpPathAndFileName -Wait
+                            Write-Verbose -Message "Deleting the RDP file after use."
+                            Remove-Item $RdpPathAndFileName -Verbose
+                            Write-Verbose -Message "Deleted $RdpPathAndFileName."
+                        }
+                        else {
+                            Write-Verbose -Message "The RDP file was not found on your desktop." -Verbose
+                        }
+                    }
+                    catch {
+                        Write-Verbose -Message "An error occurred during the creation of the RDP file."
+                        $Error[0]
+                    }
+                    finally {
+                        $ProgressPreference = 'Continue'
+                    }
+                }
+            }
+        }
+    }
+}
