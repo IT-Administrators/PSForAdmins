b0b92f125937e03232337c25393b6b4cb36b3a07
IT Admin <91905626+IT-Administrators@users.noreply.github.com>
2024-04-25 15:24:52 +0200

    Add files via upload
    This script can be deployed via Intune for example to push the bitlocker key to aad.

---
+++ PowerShell-5.1/Backup_BitlockerKeyToAAD.ps1
@@ -0,0 +1,16 @@
+﻿<#
+Author: IT-Administrators
+Powershell Version: 5.1.19041.1237
+#>
+$GetBitLockerInfos = Get-BitLockerVolume -MountPoint $env:SystemDrive
+$RecoveryProtector = $GetBitLockerInfos.KeyProtector | Where-Object { $_.KeyProtectorType -eq 'RecoveryPassword' }
+        
+foreach ($Key in $RecoveryProtector.KeyProtectorID) {
+    try {
+        BackupToAAD-BitLockerKeyProtector -MountPoint $env:SystemDrive -KeyProtectorId $Key -ErrorAction SilentlyContinue
+    }
+    catch {
+        Write-Output "Could not back up to Azure AD. Error: "
+        Write-Output $_
+    }
+}
