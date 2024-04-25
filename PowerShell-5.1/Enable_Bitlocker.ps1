c1eb79427d16d23a64584bf9846f8a72d5d55796
IT Admin <91905626+IT-Administrators@users.noreply.github.com>
2024-04-25 15:27:30 +0200

    Add files via upload
    This script can be used to enable bitlocker with TPM and save the key in azure ad.

---
+++ PowerShell-5.1/Enable_Bitlocker.ps1
@@ -0,0 +1,24 @@
+﻿<#
+Author: IT-Administrators
+Powershell Version: 5.1.19041.1237
+#>
+$BitlockerStatus = (Get-BitLockerVolume -MountPoint $env:SystemDrive).ProtectionStatus
+$BitlockerEncryptionPercentage = (Get-BitLockerVolume -MountPoint $env:SystemDrive).Encryptionpercentage
+if($BitlockerStatus -ne "On"){
+    Enable-BitLocker -MountPoint $env:SystemDrive -EncryptionMethod XtsAes256 -TpmProtector
+    Add-BitLockerKeyProtector -MountPoint $env:SystemDrive -RecoveryPasswordProtector
+}
+else{
+    $GetBitLockerInfos = Get-BitLockerVolume -MountPoint $env:SystemDrive
+    $RecoveryProtector = $GetBitLockerInfos.KeyProtector | Where-Object { $_.KeyProtectorType -eq 'RecoveryPassword' }
+        
+    foreach ($Key in $RecoveryProtector.KeyProtectorID) {
+        try {
+            BackupToAAD-BitLockerKeyProtector -MountPoint $env:SystemDrive -KeyProtectorId $Key -ErrorAction SilentlyContinue
+        }
+        catch {
+            Write-Output "Could not back up to Azure AD. Error: "
+            Write-Output $_
+        }
+    }
+}
