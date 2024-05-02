1cd9771a59f52baa98c1108ccfdac88e16c2d2c6
IT Admin <91905626+IT-Administrators@users.noreply.github.com>
2024-05-02 08:25:56 +0200

    Add files via upload
    Removes the teams cache of the current user.

---
+++ PowerShell-5.1/Remove_UserTeamsCache.ps1
@@ -0,0 +1,19 @@
+﻿<#
+Author: IT-Administrators
+Powershell Version: 5.1.19041.1237
+#>
+<#This script removes the teams user cache.#>
+powershell -WindowStyle Hidden -NoProfile -command{
+    $TeamsPath = "$Env:APPDATA\Microsoft\Teams"
+    $TeamsProcess = Get-Process -Name Teams -ErrorAction SilentlyContinue
+    if($TeamsProcess -eq $null){
+        $TeamsDirFiles = Get-ChildItem -Path $TeamsPath -Recurse | Where-Object {$_.FullName -notlike "*Backgrounds*"}
+        $TeamsDirFiles | ForEach-Object{Remove-Item -Path $_.FullName -Recurse -Force -ErrorAction SilentlyContinue}
+    }
+    else{
+        Stop-Process -Name $TeamsProcess.ProcessName -Force -Verbose
+        Wait-Process -Name $TeamsProcess.ProcessName -Verbose
+        $TeamsDirFiles = Get-ChildItem -Path $TeamsPath -Recurse | Where-Object {$_.FullName -notlike "*Backgrounds*"}
+        $TeamsDirFiles | ForEach-Object{Remove-Item -Path $_.FullName -Recurse -Force -ErrorAction SilentlyContinue}
+    }
+} 
