ed9556da4cd2a81d4d9aaec2e54673ced9a12d33
IT Admin <91905626+IT-Administrators@users.noreply.github.com>
2024-06-13 15:38:13 +0200

    Add files via upload
    Implements functions to remove PSHistory from memory and edit PSHistory configuration. For example to not save a history to file.

---
+++ PowerShell-5.1/Functions_PSHistory.ps1
@@ -0,0 +1,69 @@
+﻿<#
+.Synopsis
+    Removes current powershell history from memory.
+
+.DESCRIPTION
+    Removes powershell history form memory. This way you can not use the last entry anymore.
+
+.EXAMPLE
+    Remove history from memory.
+
+    Remove-PSHistoryMemoryRoH
+
+.NOTES
+    Written and testet in PowerShell 5.1.
+
+.LINK
+    https://github.com/IT-Administrators/PSForAdmins/tree/main/PowerShell-5.1
+#>
+
+function Remove-PSHistoryMemoryRoH {
+    
+    [CmdletBinding(DefaultParameterSetName='PSHistory', 
+               SupportsShouldProcess=$true)]
+    param(
+        [Parameter(
+        ParameterSetName='PSHistory',
+        Position=0,
+        HelpMessage='Remove PSHistory from memory.')]
+        [Switch]$RemoveMemHistroy
+    )
+
+    if($RemoveMemHistroy -or !$RemoveMemHistroy) {
+        [Microsoft.PowerShell.PSConsoleReadLine]::ClearHistory()
+    }
+}
+
+<#
+.Synopsis
+    Configure PSHistory.
+
+.DESCRIPTION
+    Configure powershell to not track history to a file anymore.
+
+.EXAMPLE
+    Configure ps to not save history to file.
+
+    Edit-PSHistoryConfigRoH -HistorySaveStyle SaveNothing
+
+.NOTES
+    Written and testet in PowerShell 5.1.
+
+.LINK
+    https://github.com/IT-Administrators/PSForAdmins/tree/main/PowerShell-5.1
+#>
+
+function Edit-PSHistoryConfigRoH {
+    
+    [CmdletBinding(DefaultParameterSetName='PSHistory', 
+               SupportsShouldProcess=$true)]
+    param(
+        [Parameter(
+        ParameterSetName='PSHistory',
+        Position=0,
+        HelpMessage='Configure PSHistory (Enable/Disable).')]
+        [Microsoft.PowerShell.HistorySaveStyle]$HistorySaveStyle = [Microsoft.PowerShell.HistorySaveStyle]::SaveIncrementally
+    )
+
+    Set-PSReadLineOption -HistorySaveStyle $HistorySaveStyle
+}
