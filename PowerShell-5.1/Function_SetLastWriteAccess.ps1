3d5a6b1f2ab36a0c2969b8d67b35c7c9e7507e53
IT-Administrators <91905626+IT-Administrators@users.noreply.github.com>
2024-08-03 11:00:27 +0200

    Added function to change LastWriteTime property
    Added a function to change LastWriteTime property on
    the files specified. This way autoremoval based on
    LastWriteTime property can be bypassed.

---
+++ PowerShell-5.1/Function_SetLastWriteAccess.ps1
@@ -0,0 +1,51 @@
+<#
+.Synopsis
+    Changes LastWriteTime property.
+
+.DESCRIPTION
+    Changes the LastWriteTime Property of the specified files.
+
+.EXAMPLE
+    Set LastWriteTime to specified date.
+
+    Set-LastWriteTimeRoH -FileName $File -SetLastWriteTimeTo "3/8/2004"
+
+.EXAMPLE
+    Set LastWriteTime of bulk files to specified date and time.
+
+    Set-LastWriteTimeRoH -FileName $Files -SetLastWriteTimeTo "3/8/2004 09:47"
+
+.NOTES
+    Written and testet in PowerShell 5.1.
+
+.LINK
+    https://github.com/IT-Administrators/PSForAdmins/tree/main/PowerShell-5.1
+#>
+
+function Set-LastWriteTimeRoH {
+    
+    [CmdletBinding(DefaultParameterSetName='SetLastWriteTime', 
+    SupportsShouldProcess=$true)]
+    
+    param(
+        [Parameter(
+        ParameterSetName='SetLastWriteTime',
+        Position=0,
+        ValueFromPipeline,
+        HelpMessage='Filename.')]
+        [String[]]$FileName,
+
+        [Parameter(
+        ParameterSetName='SetLastWriteTime',
+        Position=0,
+        HelpMessage='Last write date.')]
+        [datetime]$SetLastWriteTimeTo
+    )
+    # Resolve file to process file information.
+    # The file needs to be resolved because FileName parameter is from type string.
+    $FileInfo = Get-ChildItem -Path $FileName
+    # Change LastWriteTime property.
+    $FileInfo | ForEach-Object{
+        $_.LastWriteTime = $SetLastWriteTimeTo
+    }
+}
