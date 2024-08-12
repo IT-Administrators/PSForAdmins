e60d3c4c3c3d08a491a6e939d993c7407acee6bf
IT Admin <91905626+IT-Administrators@users.noreply.github.com>
2024-08-12 14:08:18 +0200

    Add files via upload
    Get kerberos info of the current user.

---
+++ PowerShell-5.1/Function_GetKerberosInfosCurrentUser.ps1
@@ -0,0 +1,34 @@
+﻿<#
+.Synopsis
+    Get kerberos token infos of curent user.
+
+.DESCRIPTION
+    Get kerberos token infos of curent user.
+
+.EXAMPLE
+    Get kerberos token infos.
+
+    Get-KerberosInfoCurrentUserRoH
+
+    Output:
+
+    AuthenticationType : CloudAP
+    ImpersonationLevel : None
+    IsAuthenticated    : True
+    IsGuest            : False
+    IsSystem           : False
+    IsAnonymous        : False
+    ...
+
+.NOTES
+    Written and testet in PowerShell 5.1.
+
+.LINK
+    https://github.com/IT-Administrators/PSForAdmins/tree/main/PowerShell-5.1
+#>
+
+function Get-KerberosInfoCurrentUserRoH {
+    
+    $Token = [System.Security.Principal.WindowsIdentity]::GetCurrent()
+    $Token
+}
