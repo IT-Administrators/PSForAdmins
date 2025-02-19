87678e0f3ebae0732c557f6ca5e75e0b519228bd
IT-Administrators <91905626+IT-Administrators@users.noreply.github.com>
2025-02-19 09:28:09 +0100

    Changed encoding/decoding B64 functions
    Added encoding parameter to Function_InvokeCommandDecodingB64 and
    Function_InvokeCommandEncodingB64 to use different encodings.
    
    This way JWT tokens can be decoded using ASCII as parameter.

--- PowerShell-5.1/Function_InvokeCommandDecodingB64.ps1
+++ PowerShell-5.1/Function_InvokeCommandDecodingB64.ps1
@@ -5,10 +5,21 @@
 .DESCRIPTION
     Converts the provided command from base64 to human readable string. 
 
+    You can specify differetn encodings to encode JWT tokens as well. 
+
 .EXAMPLE
+    Decode command using default encoding.
+
     Invoke-CommandDecodingB64RoH -Command "SABlAGwAbABvACAAVwBvAHIAbABkACEA"
 
     Hello World!
+
+.EXAMPLE
+    Decode command using ASCII encoding.
+    
+    Invoke-CommandDecodingB64RoH -Command "SABlAGwAbABvACAAVwBvAHIAbABkACEA" -Encoding ASCII
+
+    H e l l o  W o r l d !
 
 .NOTES
     Written and testet in PowerShell 5.1.
@@ -26,8 +37,15 @@ function Invoke-CommandDecodingB64RoH {
         ParameterSetName='DecodeCommand',
         Position=0,
         HelpMessage='Command to decode. Default command is base64 representation of Hello World!')]
-        [string]$Command = "SABlAGwAbABvACAAVwBvAHIAbABkACEA"
+        [string]$Command = "SABlAGwAbABvACAAVwBvAHIAbABkACEA",
+
+        [Parameter(
+        ParameterSetName='DecodeCommand',
+        Position=0,
+        HelpMessage='Encoding to be used.')]
+        [ValidateSet("Unicode","ASCII","UTF8","UTF7","UTF32","BigEndianUnicode")]
+        [string]$Encoding = "Unicode"
     )
 
-    [System.Text.Encoding]::Unicode.GetString([System.Convert]::FromBase64String($Command))
+    [System.Text.Encoding]::$Encoding.GetString([System.Convert]::FromBase64String($Command))
 }
--- PowerShell-5.1/Function_InvokeCommandEncodingB64.ps1
+++ PowerShell-5.1/Function_InvokeCommandEncodingB64.ps1
@@ -6,9 +6,18 @@
     Converts the provided command to base64 using the .net class.
 
 .EXAMPLE
+    Encode command using default encoding.
+
     Invoke-CommandEncodingB64RoH -Command "Hello World!"
 
     SABlAGwAbABvACAAVwBvAHIAbABkACEA
+
+.EXAMPLE
+    Encode command using ASCII encoding.
+
+    Invoke-CommandEncodingB64RoH -Command "Hello World!" -Encoding ASCII
+
+    SGVsbG8gV29ybGQh
 
 .NOTES
     Written and testet in PowerShell 5.1.
@@ -26,10 +35,17 @@ function Invoke-CommandEncodingB64RoH {
         ParameterSetName='EncodeCommand',
         Position=0,
         HelpMessage='Command to encode.')]
-        [String]$Command = "Hello World!"
+        [String]$Command = "Hello World!",
+
+        [Parameter(
+        ParameterSetName='EncodeCommand',
+        Position=0,
+        HelpMessage='Encoding to be used.')]
+        [ValidateSet("Unicode","ASCII","UTF8","UTF7","UTF32","BigEndianUnicode")]
+        [string]$Encoding = "Unicode"
     )
 
-    $Bytes = [System.Text.Encoding]::Unicode.GetBytes($Command)
+    $Bytes = [System.Text.Encoding]::$Encoding.GetBytes($Command)
     $EncodedCommand = [Convert]::ToBase64String($Bytes)
     $EncodedCommand
 }
