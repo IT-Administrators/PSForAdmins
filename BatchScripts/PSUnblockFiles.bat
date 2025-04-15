d709fd36f279443be821f51a59f732eef6ea16f1
IT-Administrators <91905626+IT-Administrators@users.noreply.github.com>
2025-04-15 13:04:05 +0200

    Changed script
    Changed comments in BatchScripts/PSUnblockFiles.bat

--- BatchScripts/PSUnblockFiles.bat
+++ BatchScripts/PSUnblockFiles.bat
@@ -1,5 +1,6 @@
 @REM This batch script runs a powershell command to unblock all files in the specified directory.
 @REM
+@REM Usage: scriptname.bat <filename>
 @REM Turn of command echoing.
 @echo off
 powershell.exe -command "Get-ChildItem -Path %1 | Unblock-File -Verbose"
