0135f0c6cf19a4a1516e81ec634de65fe3566c90
IT-Administrators <91905626+IT-Administrators@users.noreply.github.com>
2025-07-03 09:03:22 +0200

    Update Function_ConnectBastionHostRDP.ps1
    Changed the Header part for the webrequest.
    
    As this scirpt is written for powershell 5.1 the bearer token must be provided in the request header. This only supports plain text string. While Get-AzAccessToken returns a SecureString this must be resolved bevore using in the authentication header.
    
    On powershell 7.0+ the Authentication parameter of the Invoke-Webrequest can be used which supports SecureStrings by default for authentication.

--- PowerShell-5.1/Function_ConnectBastionHostRDP.ps1
+++ PowerShell-5.1/Function_ConnectBastionHostRDP.ps1
@@ -26,175 +26,180 @@
     VERBOSE: Performing the operation "Remove File" on target "%userprofile%\Desktop\ExampleVM-2024-04-16@140441.rdp".
     VERBOSE: Deleted %userprofile%\Desktop\ExampleVM-2024-04-16@140441.rdp.
 
-.EXAMPLE
-    Connect to bastion host with all monitors used.
-
-    Connect-AzureBastionHostRDP -AzureVmName "ExampleVM" -BastionHostName "Example-Bastion" -TenantId "xxxxxxxx" -BastionSubscriptionId "xxxxxxxx" -BastionResourceGroup "ExampleDomain-RSG-Bastion" -UseMultimonitor
-
 .NOTES
-    Written and tested in PowerShell 5.1.
+    Written and testet in PowerShell 5.1.
 
 .LINK
     https://github.com/IT-Administrators/PSForAdmins/tree/main/PowerShell-5.1
 #>
 
-function Connect-AzureBastionHostRDP {
+[CmdletBinding(DefaultParameterSetName='ConnectAzureBastionHost', 
+                SupportsShouldProcess=$true)]
 
-    [CmdletBinding(DefaultParameterSetName='ConnectAzureBastionHost', 
-                   SupportsShouldProcess=$true)]
+param(
+    [Parameter(
+    ParameterSetName='ConnectAzureBastionHost',
+    Position=0,
+    HelpMessage='Azure VM name.')]
+    [ValidateNotNullOrEmpty()]
+    [string]$AzureVmName,
 
-    param(
-        [Parameter(
-        ParameterSetName='ConnectAzureBastionHost',
-        Position=0,
-        HelpMessage='Azure VM name.')]
-        [ValidateNotNullOrEmpty()]
-        [string]$AzureVmName,
+    [Parameter(
+    ParameterSetName='ConnectAzureBastionHost',
+    Position=1,
+    HelpMessage='Bastion hostname.')]
+    [string]$BastionHostName,
 
-        [Parameter(
-        ParameterSetName='ConnectAzureBastionHost',
-        Position=0,
-        HelpMessage='Bastion hostname.')]
-        [string]$BastionHostName,
+    [Parameter(
+    ParameterSetName='ConnectAzureBastionHost',
+    Position=2,
+    HelpMessage='TenantId.')]
+    [string]$TenantId,
 
-        [Parameter(
-        ParameterSetName='ConnectAzureBastionHost',
-        Position=0,
-        HelpMessage='TenantId.')]
-        [string]$TenantId,
+    [Parameter(
+    ParameterSetName='ConnectAzureBastionHost',
+    Position=3,
+    HelpMessage='Bastion subscription id.')]
+    [string]$BastionSubscriptionId,
 
-        [Parameter(
-        ParameterSetName='ConnectAzureBastionHost',
-        Position=0,
-        HelpMessage='Bastion subscription id.')]
-        [string]$BastionSubscriptionId,
+    [Parameter(
+    ParameterSetName='ConnectAzureBastionHost',
+    Position=4,
+    HelpMessage='Bastion resource group.')]
+    [string]$BastionResourceGroup,
 
-        [Parameter(
-        ParameterSetName='ConnectAzureBastionHost',
-        Position=0,
-        HelpMessage='Bastion resource group.')]
-        [string]$BastionResourceGroup,
+    [Parameter(
+    ParameterSetName='ConnectAzureBastionHost',
+    Position=5,
+    HelpMessage='Multimonitor or singlemonitor setup.')]
+    [switch]$UseMultimonitor,
 
-        [Parameter(
-        ParameterSetName='ConnectAzureBastionHost',
-        Position=0,
-        HelpMessage='User multimonitor or single.')]
-        [switch]$UseMultimonitor,
+    [Parameter(
+    ParameterSetName='ConnectAzureBastionHostModuleCheck',
+    Position=0,
+    HelpMessage='Required modules will be installed or updated.')]
+    [switch]$CheckPrerequisites
+)
 
-        [Parameter(
-        ParameterSetName='ConnectAzureBastionHostModuleCheck',
-        Position=0,
-        HelpMessage='Required modules will be installed or updated.')]
-        [switch]$CheckPrerequisites
-    )
-
-    if ($CheckPrerequisites){
-        # Necessary modules for the RDP connection.
-        $Modules = @("Az.accounts","Az.ResourceGraph","Az.Network")
-        # Verify that modules are installed.
-        foreach($module in $Modules){
-            $ModuleCheck = Get-Module -Name $module -ListAvailable -All
-            if ($ModuleCheck -ne $true){
-                Find-Module -Name $module -Verbose | Install-Module -Scope CurrentUser -Verbose -Force -Confirm:$false
-                Import-Module -Name $module -Scope Local -Verbose
-            }
-            else{
-                Update-Module -Name $module -Verbose
-                Import-Module -Name $module -Scope Local -Verbose
-            }
+if ($CheckPrerequisites){
+    # Necessary modules for the RDP connection.
+    $Modules = @("Az.accounts","Az.ResourceGraph","Az.Network")
+    Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser -Verbose
+    # Verify that modules are installed.
+    foreach($module in $Modules){
+        $ModuleCheck = Get-Module -Name $module -ListAvailable -All
+        if ($ModuleCheck -ne $true){
+            Find-Module -Name $module -Verbose | Install-Module -Scope CurrentUser -Verbose -Force
+            Import-Module -Name $module -Scope Local -Verbose
+        }
+        else{
+            Update-Module -Name $module -Verbose
+            Import-Module -Name $module -Scope Local -Verbose
         }
     }
-    else{
-        # Import-Module -Name $Modules -Verbose
-        Connect-AzAccount -Tenant $TenantId -Subscription $BastionSubscriptionId | Out-Null
+}
+else{
+    # Set executionpolicy to remotesigned to be able to import modules without any problem.
+    Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser -Verbose
+    
+    # Import-Module -Name $Modules -Verbose
+    Connect-AzAccount -Tenant $TenantId -Subscription $BastionSubscriptionId | Out-Null
+        
+    # Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser -Verbose
 
-        # Query azure to find vm and get information about it.
-        $VMToConnectTo = Search-AzGraph -Query "resources | extend PowerState = tostring(properties.extended.instanceView.powerState.code) | where type == 'microsoft.compute/virtualmachines' and name == '$AzureVmName'" -UseTenantScope
-        $VmResourceId = $VMToConnectTo.ResourceId
+    # Query azure to find vm and get information about it.
+    $VMToConnectTo = Search-AzGraph -Query "resources | extend PowerState = tostring(properties.extended.instanceView.powerState.code) | where type == 'microsoft.compute/virtualmachines' and name == '$AzureVmName'" -UseTenantScope
+    $VmResourceId = $VMToConnectTo.ResourceId
 
-        if (!([string]::IsNullOrEmpty($VmResourceId))) {
-            Write-Verbose -Message "Found the VM." -Verbose
-            # Check if machine is running otherwise exit.
-            if ($VMToConnectTo.PowerState -eq 'PowerState/deallocated') {
-                $User = [Environment]::UserName
-                Write-Verbose -Message "The VM is not running. Please start the VM." -Verbose
-            }
-            else {
-                # Connect to the bastion sub in the correct tenant.
-                Set-AzContext -SubscriptionId $BastionSubscriptionId -Tenant $TenantId 
-                # Select-AzSubscription -SubscriptionId $BastionSubscriptionId -Tenant $TenantId | Out-Null
-                # Get azure access token.
-                $AccessToken = (Get-AzAccessToken -AsSecureString).Token
-                if (!([string]::IsNullOrEmpty($AccessToken))) {
-                    try {
-                        # Get bastion information.
-                        $Bastion = Get-AzBastion -ResourceGroupName $BastionResourceGroup -Name $BastionHostName
-                        if ($null -ne $Bastion) {
-                            Write-Verbose -Message "Connected to Bastion $($Bastion.Name)" -Verbose
-                            Write-Verbose -Message "Creating RDP profile." -Verbose
+    if (!([string]::IsNullOrEmpty($VmResourceId))) {
+        Write-Verbose -Message "Found the VM." -Verbose
+        # Check if machine is running otherwise exit.
+        if ($VMToConnectTo.PowerState -eq 'PowerState/deallocated') {
+            $User = [Environment]::UserName
+            Write-Verbose -Message "The Vm is not running. Please start the vm." -Verbose
+        }
+        else {
+            # Connect to the bastion sub in the correct tenant.
+            Set-AzContext -SubscriptionId $BastionSubscriptionId -Tenant $TenantId 
+            # Select-AzSubscription -SubscriptionId $BastionSubscriptionId -Tenant $TenantId | Out-Null
+            # Get azure access token. The token is in securestring format now.
+            # It must be resolved to use it as bearer token. 
+            $AccessToken = (Get-AzAccessToken -AsSecureString).Token
+            if (!([string]::IsNullOrEmpty($AccessToken))) {
+                try {
+                    # Get bastion information.
+                    $Bastion = Get-AzBastion -ResourceGroupName $BastionResourceGroup -Name $BastionHostName
+                    if ($null -ne $Bastion) {
+                        Write-Verbose -Message "Connected to Bastion $($Bastion.Name)" -Verbose
+                        Write-Verbose -Message "Creating RDP profile." -Verbose
                             
-                            # Create endpoint (url) informations. This is later used to create the rdp file.
-                            $TargetResourceId = $VmResourceId
-                            $EnableMFA = "true"
-                            $BastionEndpoint = $Bastion.DnsName
-                            $ResourcePort = "3389"
+                        # Create endpoint (url) informations. This is later used to create the rdp file.
+                        $TargetResourceId = $VmResourceId
+                        $EnableMFA = "true"
+                        $BastionEndpoint = $Bastion.DnsName
+                        $ResourcePort = "3389"
 
-                            $Url = "https://$($BastionEndpoint)/api/rdpfile?resourceId=$($TargetResourceId)&format=rdp&rdpport=$($ResourcePort)&enablerdsaad=$($EnableMFA)"
+                        $Url = "https://$($BastionEndpoint)/api/rdpfile?resourceId=$($TargetResourceId)&format=rdp&rdpport=$($ResourcePort)&enablerdsaad=$($EnableMFA)"
 
-                            # Create authentication headers.
-                            $Headers = @{
-                                "Authorization"   = "Bearer $($AccessToken)"
-                                "Accept"          = "*/*"
-                                "Accept-Encoding" = "gzip, deflate, br"
-                                "Content-Type"    = "application/json"
-                            }
-                            # Create RDP file informations.
-                            $DesktopPath = [Environment]::GetFolderPath("Desktop")
-                            $DateStamp = Get-Date -Format yyyy-MM-dd
-                            $TimeStamp = Get-Date -Format HHmmss
-                            $DateAndTimeStamp = $DateStamp + '@' + $TimeStamp 
-                            $RdpPathAndFileName = "$DesktopPath\$AzureVmName-$DateAndTimeStamp.rdp"
-                            $ProgressPreference = 'SilentlyContinue'
+                        # Convert SecureString back to plain text for use. This must be done because the bearer token is specified in the header.
+                        # In Powershell 7.0+ the Authentication parameter can be used. This doesnt require resolving the SecureString.
+                        $PlainToken = [Runtime.InteropServices.Marshal]::PtrToStringAuto(
+                            [Runtime.InteropServices.Marshal]::SecureStringToBSTR($AccessToken)
+                        )
+
+                        # Create authentication headers.
+                        $Headers = @{
+                            "Authorization"   = "Bearer $($PlainToken)"
+                            "Accept"          = "*/*"
+                            "Accept-Encoding" = "gzip, deflate, br"
+                            #"Connection" = "keep-alive" #keep-alive and close not supported with PoSh 5.1 
+                            "Content-Type"    = "application/json"
                         }
-                        else {
-                            Write-Verbose -Message "We could not connect to azure bastion host." -Verbose
-                        }
-                    }
-                    catch {
-                        Write-Error -Message "Internal error." -Category ObjectNotFound
-                    }
-                    
-                    try {
+                        # Create RDP file informations.
+                        $DesktopPath = [Environment]::GetFolderPath("Desktop")
+                        $DateStamp = Get-Date -Format yyyy-MM-dd
+                        $TimeStamp = Get-Date -Format HHmmss
+                        $DateAndTimeStamp = $DateStamp + '@' + $TimeStamp 
+                        $RdpPathAndFileName = "$DesktopPath\$AzureVmName-$DateAndTimeStamp.rdp"
                         $ProgressPreference = 'SilentlyContinue'
-                        # Get RDP configfile informations.
-                        Invoke-WebRequest $Url -Method Get -Headers $Headers -OutFile $RdpPathAndFileName -UseBasicParsing
-                        $ProgressPreference = 'Continue'
+                    }
+                    else {
+                        Write-Verbose -Message "We could not connect to azure bastion host." -Verbose
+                    }
+                }
+                catch {
+                    Write-Error -Message "Internal error." -Category ObjectNotFound
+                }
+                    
+                try {
+                    $ProgressPreference = 'SilentlyContinue'
+                    # Get RDP configfile informations.
+                    Invoke-WebRequest $Url -Method Get -Headers $Headers -OutFile $RdpPathAndFileName -UseBasicParsing
+                    $ProgressPreference = 'Continue'
                         
-                        # Check if RDP config file exist and if yes start it.
-                        if (Test-Path $RdpPathAndFileName -PathType Leaf) {
-                            # Change from multimonitor to single.
-                            if($UseMultimonitor -eq $false){
-                                $RdpFileContent = Get-Content -Path $RdpPathAndFileName -Raw
-                                $RdpFileContent.Replace("use multimon:i:1","use multimon:i:0") | Add-Content -Path $RdpPathAndFileName -Force -Verbose
-                            }
-                            
-                            Start-Process $RdpPathAndFileName -Wait
-                            Write-Verbose -Message "Deleting the RDP file after use." -Verbose
-                            
-                            Remove-Item $RdpPathAndFileName -Verbose
-                            Write-Verbose -Message "Deleted $RdpPathAndFileName." -Verbose
+                    # Check if RDP config file exist and if yes start it.
+                    if (Test-Path $RdpPathAndFileName -PathType Leaf) {
+                        # Change from multimonitor to single.
+                        if($UseMultimonitor -eq $false){
+                            $RdpFileContent = Get-Content -Path $RdpPathAndFileName -Raw
+                            $RdpFileContent.Replace("use multimon:i:1","use multimon:i:0") | Add-Content -Path $RdpPathAndFileName -Force -Verbose
                         }
-                        else {
-                            Write-Verbose -Message "The RDP file was not found on your desktop." -Verbose
-                        }
+                        Start-Process $RdpPathAndFileName -Wait
+                        Write-Verbose -Message "Deleting the RDP file after use." -Verbose
+                        Remove-Item $RdpPathAndFileName -Verbose
+                        # Write-Verbose -Message "Deleted $RdpPathAndFileName." -Verbose
+                        Set-ExecutionPolicy -ExecutionPolicy Default -Scope CurrentUser -Verbose
                     }
-                    catch {
-                        Write-Verbose -Message "An error occurred during the creation of the RDP file."
-                        $Error[0]
+                    else {
+                        Write-Verbose -Message "The RDP file was not found on your desktop." -Verbose
                     }
-                    finally {
-                        $ProgressPreference = 'Continue'
-                    }
+                }
+                catch {
+                    Write-Verbose -Message "An error occurred during the creation of the RDP file." -Verbose
+                    $Error[0]
+                }
+                finally {
+                    $ProgressPreference = 'Continue'
                 }
             }
         }
