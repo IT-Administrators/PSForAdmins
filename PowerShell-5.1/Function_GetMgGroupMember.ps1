2f99efc12514cca6bfba7396c6b0c023e4eff5e6
IT-Administrators <91905626+IT-Administrators@users.noreply.github.com>
2025-10-07 12:28:43 +0200

    Update Function_GetMgGroupMember.ps1
    Added functionality to filter for only direct members of a group or only subgroups.

--- PowerShell-5.1/Function_GetMgGroupMember.ps1
+++ PowerShell-5.1/Function_GetMgGroupMember.ps1
@@ -1,29 +1,35 @@
-﻿<#
+function Get-MgGroupMemberRoH {
+<#
 .Synopsis
-    Get all group members of specified group.
+    Get members of the specified group.
 
 .DESCRIPTION
+    Get members of the specified group.
+
     Requires permissions:
     - group.readwrite.all,user.read.all
 
-    Get all group members of specified group with additional information.
-
-    The cmdlet Get-MgGroupMember only returns the userids. This function
+    The cmdlet <Get-MgGroupMember> only returns the userids. This function
     resolves the ids in the same step.
 
-.EXAMPLE
-    Get group members of specified group.
+    Using the <OnlyUsers> switch gets the direct members of the group.
+    The <OnlyGroups> switch gets all members of the current group which are itself a group.
+    No parameter except <GroupId> gets all members. 
 
-    Get-MgGroupMemberRoH -GroupID 01168566-1636-4922-b345-7cbe6105149e
+.EXAMPLE
+    Get members of the specified group.
+
+    Get-MgGroupMemberRoH -GroupID 0098c5df-3999-4b90-a0ab-817898a38469
 
     Output:
 
-    DisplayName     Id                                   Mail                             UserPrincipalName         
-    -----------     --                                   ----                             -----------------         
-    ExampleUser1    452e0ed2-f9a4-4196-b957-7b7538fe8de5 ExampleUser1@ExampleDomain.com   ExampleUser1@ExampleDomain.com  
-    ExampleUser2    82857ae9-7c36-46ce-95bb-381b84369f90 ExampleUser2@ExampleDomain.com   ExampleUser2@ExampleDomain.com  
-    ExampleUser3    2231d223-b1c3-43b5-a71f-f71ffc937cfe ExampleUser3@ExampleDomain.com   ExampleUser3@ExampleDomain.com
-    ExampleUser4    f7ab2add-fd67-4afe-9e81-2ffbe86eb61d ExampleUser4@ExampleDomain.com   ExampleUser4@ExampleDomain.com   
+    DisplayName   Id                                   Mail                           UserPrincipalName
+    -----------   --                                   ----                           -----------------
+    Example, User 10cf779b-600c-40e0-b2b9-fb0288371be3 Example.User@ExampleDomain.com Example.User@ExampleDomain.com
+
+    DisplayName   Id                                   MailNickname  Description  GroupTypes
+    -----------   --                                   ------------  -----------  ----------
+    Example Group dbd69841-5adf-43c6-a63a-6cbc951446a4 ExampleGoup   ExampleGroup {}
     ...
 
 .NOTES
@@ -33,22 +39,66 @@
     https://github.com/IT-Administrators/PSForAdmins/tree/main/PowerShell-5.1
 #>
 
-function Get-MgGroupMemberRoH {
-
-    [CmdletBinding(DefaultParameterSetName='GetMgGroupMember', 
+    [CmdletBinding(DefaultParameterSetName='MgGroupMemberUsers', 
                    SupportsShouldProcess=$true)]
-    
     param(
         [Parameter(
-        ParameterSetName='GetMgGroupMember',
+        ParameterSetName='MgGroupMemberUsers', Position=0, HelpMessage='Group id.')]
+        [Parameter(
+        ParameterSetName='MgGroupMemberGroups', Position=0, HelpMessage='Group id.')]
+        [guid]$GroupID,
+
+        [Parameter(
+        ParameterSetName='MgGroupMemberUsers',
         Position=0,
-        HelpMessage='Group id.')]
-        [String]$GroupID
+        HelpMessage='Get only users.')]
+        [switch]$OnlyUsers,
+
+        [Parameter(
+        ParameterSetName='MgGroupMemberGroups',
+        Position=0,
+        HelpMessage='Get only groups.')]
+        [switch]$OnlyGroups
     )
     
-    $MGGroup = Get-MgGroup -GroupId $GroupID
-    $MGGroupMember = Get-MgGroupMember -GroupId $MGGroup.Id
-    $MGGroupMember | ForEach-Object {
-        Get-MgUser -UserId $_.id
+    begin {
+        
+    }
+    
+    process {
+        $MGGroup = Get-MgGroup -GroupId $GroupID
+        $MGGroupMember = Get-MgGroupMember -GroupId $MGGroup.Id -All
+        if ($OnlyUsers) {
+            $MgGroupMember | ForEach-Object {
+                # Filter only for users.
+                if($_.AdditionalProperties["@odata.type"] -eq "#microsoft.graph.user") {
+                    Get-MgUser -UserId $_.Id
+                }
+            }
+        }
+        if ($OnlyGroups) {
+            $MgGroupMember | ForEach-Object {
+                # Filter only for groups.
+                if($_.AdditionalProperties["@odata.type"] -eq "#microsoft.graph.group") {
+                    Get-MgGroup -GroupId $_.Id
+                }
+            }
+        }
+        if (!$OnlyUsers -and !$OnlyGroups) {
+            $MgGroupMember | ForEach-Object {
+                # Filter only for users.
+                if($_.AdditionalProperties["@odata.type"] -eq "#microsoft.graph.user") {
+                    Get-MgUser -UserId $_.Id
+                }
+                # Filter only for groups.
+                if($_.AdditionalProperties["@odata.type"] -eq "#microsoft.graph.group") {
+                    Get-MgGroup -GroupId $_.Id
+                }
+            }
+        }
+    }
+    
+    end {
+        
     }
 }
